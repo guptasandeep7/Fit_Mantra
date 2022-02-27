a295f7dde5ae8734a813840e73ac39fcac1e14d9
Sandeep Gupta <guptasg300@gmail.com>
2022-02-27 08:15:22 +0530

    Back Fragment completed

--- app/src/main/java/com/example/morefit/cal/BmiFragment.kt
+++ app/src/main/java/com/example/morefit/cal/BmiFragment.kt
@@ -5,12 +5,50 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.morefit.databinding.FragmentBmiBinding
+import kotlin.math.floor
+import kotlin.math.pow
 
 class BmiFragment : Fragment() {
 
     private var _binding: FragmentBmiBinding? = null
     private val binding get() = _binding!!
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.calculateBtn.setOnClickListener {
+            helper()
+            if (isValid()) {
+                val weight = binding.weight.text.toString().toDouble()
+                val height = binding.height.text.toString().toDouble()
+                val bmi = weight / height.pow(2.0)
+                binding.bmi.text = "Your BMI is \n${floor(bmi)}"
+            }
+        }
+
+        binding.backBtn.setOnClickListener { findNavController().navigateUp() }
+    }
+
+    private fun helper() {
+        binding.weightLayout.helperText = ""
+        binding.heightLayout.helperText = ""
+    }
+
+    private fun isValid(): Boolean {
+        return when {
+            binding.weight.text.isNullOrBlank() -> {
+                binding.weightLayout.helperText = "Please enter weight"
+                false
+            }
+            binding.height.text.isNullOrBlank() -> {
+                binding.heightLayout.helperText = "Please enter height"
+                false
+            }
+            else -> true
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
--- app/src/main/java/com/example/morefit/home/BackFragment.kt
+++ app/src/main/java/com/example/morefit/home/BackFragment.kt
@@ -5,16 +5,74 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import com.example.morefit.R
+import com.example.morefit.databinding.FragmentBackBinding
+import com.example.morefit.databinding.FragmentFrontBinding
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
-class BackFragment : Fragment() {
+class BackFragment : Fragment(),View.OnClickListener {
 
+    private var _binding: FragmentBackBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.backTop.setOnClickListener(this)
+        binding.backRightBiceps.setOnClickListener(this)
+        binding.backLeftBiceps.setOnClickListener(this)
+        binding.backLeftShoulder.setOnClickListener(this)
+        binding.backRightShoulder.setOnClickListener(this)
+        binding.backRightArm.setOnClickListener(this)
+        binding.backLeftArm.setOnClickListener(this)
+        binding.backRightLats.setOnClickListener(this)
+        binding.backLeftLats.setOnClickListener(this)
+        binding.backRightThigh.setOnClickListener(this)
+        binding.backLeftThigh.setOnClickListener(this)
+        binding.backRightLeg.setOnClickListener(this)
+        binding.backLeftLeg.setOnClickListener(this)
+        binding.backLowerBack.setOnClickListener(this)
+        binding.backGlutes.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View?) {
+        when(view?.id){
+            R.id.back_top -> view.setBackgroundResource(R.drawable.back_top)
+            R.id.back_right_biceps -> view.setBackgroundResource(R.drawable.back_right_biceps)
+            R.id.back_left_biceps -> view.setBackgroundResource(R.drawable.back_left_biceps)
+            R.id.back_left_shoulder -> view.setBackgroundResource(R.drawable.back_left_shoulder)
+            R.id.back_right_shoulder -> view.setBackgroundResource(R.drawable.back_right_shoulder)
+            R.id.back_right_arm -> view.setBackgroundResource(R.drawable.back_right_arm)
+            R.id.back_left_arm -> view.setBackgroundResource(R.drawable.back_left_arm)
+            R.id.back_right_lats -> view.setBackgroundResource(R.drawable.back_right_lats)
+            R.id.back_left_lats -> view.setBackgroundResource(R.drawable.back_left_lats)
+            R.id.back_right_thigh -> view.setBackgroundResource(R.drawable.back_right_thigh)
+            R.id.back_left_thigh -> view.setBackgroundResource(R.drawable.back_left_thigh)
+            R.id.back_right_leg -> view.setBackgroundResource(R.drawable.back_right_leg)
+            R.id.back_left_leg -> view.setBackgroundResource(R.drawable.back_left_leg)
+            R.id.back_lower_back -> view.setBackgroundResource(R.drawable.back_lower_back)
+            R.id.back_glutes -> view.setBackgroundResource(R.drawable.back_glutes)
+        }
+        lifecycleScope.launch {
+            delay(100)
+            findNavController().navigate(R.id.action_homefragment_to_exerciseFragment)
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_back, container, false)
+        _binding = FragmentBackBinding.inflate(inflater, container, false)
+        return binding.root
     }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
 }
