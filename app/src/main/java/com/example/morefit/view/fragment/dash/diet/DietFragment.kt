9b363ef05fc2efaddc06e0be5a6961b4d2449dc9
Jyotsna <guptajyotsna2002@gmail.com>
2022-07-25 18:23:27 +0530

    Diet template,Diet queries done

--- app/src/main/java/com/example/morefit/view/fragment/dash/diet/DietFragment.kt
+++ app/src/main/java/com/example/morefit/view/fragment/dash/diet/DietFragment.kt
@@ -1,25 +1,88 @@
 package com.example.morefit.view.fragment.dash.diet
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import android.widget.Toast
 import com.example.morefit.R
+import com.example.morefit.databinding.FragmentDietBinding
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
+import java.util.*
 
-class DietFragment : Fragment() {
+class DietFragment : Fragment(R.layout.fragment_diet), View.OnClickListener {
+    private lateinit var binding: FragmentDietBinding
+    private val bottomSheetDialog by lazy { BottomSheetDialog(requireContext()) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_diet, container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentDietBinding.bind(view)
+        binding.GlutenFree.setOnClickListener(this)
+        binding.ketogenic.setOnClickListener(this)
+        binding.Vegan.setOnClickListener(this)
+        binding.LactoVegetarian.setOnClickListener(this)
+        binding.OvoVegetarian.setOnClickListener(this)
+        binding.Whole30.setOnClickListener(this)
+    }
+    private fun showBottomSheet(){
+        val items =
+            layoutInflater.inflate(R.layout.dialog_set_calories, null)
+        val timeFrame=layoutInflater.inflate(R.layout.dialog_set_time_frame, null)
+        bottomSheetDialog.setContentView(items)
+        bottomSheetDialog.show()
+        val calory=items.findViewById<TextInputEditText>(R.id.calory)
+        val calLay=items.findViewById<TextInputLayout>(R.id.calory_layout)
+        val autoComplete=timeFrame.findViewById<AutoCompleteTextView>(R.id.autoCompleteTextView)
+        val caloryBtn=items.findViewById<MaterialButton>(R.id.enter_btn)
+        val selectBtn=timeFrame.findViewById<MaterialButton>(R.id.select_btn)
+        val frameItems= listOf("Day","Week")
+
+        caloryBtn.setOnClickListener {
+            val cal=calory.text.toString()
+            if (cal == ""){
+                calLay.helperText="Minimum calories should be 500"
+            }
+            else if (cal.toInt() >= 500){
+                bottomSheetDialog.setContentView(timeFrame)
+                bottomSheetDialog.show()
+                val adapter= context?.let { ArrayAdapter(it,R.layout.list_items,frameItems) }
+                autoComplete.setAdapter(adapter)
+                selectBtn.setOnClickListener{
+                    Toast.makeText(context,
+                        autoComplete.text.toString().lowercase(), Toast.LENGTH_SHORT).show()
+                }
+            }
+            else{
+                calLay.helperText="Minimum calories should be 500"
+            }
+        }
     }
 
+    override fun onClick(v: View?) {
+        if (v != null) {
+            when(v.id){
+                R.id.GlutenFree->showBottomSheet()
+                R.id.ketogenic->showBottomSheet()
+                R.id.Vegan->showBottomSheet()
+                R.id.LactoVegetarian->showBottomSheet()
+                R.id.OvoVegetarian->showBottomSheet()
+                R.id.Whole30->showBottomSheet()
+
+            }
+        }
+    }
 }
