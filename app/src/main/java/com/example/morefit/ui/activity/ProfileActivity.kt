e2e221cb82ae515a48fd66a34d442fcbdac27191
Debashish Kundu <debashish.joy@gmail.com>
2022-07-26 12:39:10 +0530

    added icons for each section in the app

--- app/src/main/java/com/example/morefit/ui/activity/ProfileActivity.kt
+++ app/src/main/java/com/example/morefit/ui/activity/ProfileActivity.kt
@@ -1,13 +1,13 @@
 package com.example.morefit.ui.activity
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import com.example.morefit.R
 import com.example.morefit.databinding.ActivityProfileBinding
+import com.google.android.material.transition.platform.MaterialArcMotion
 import com.google.android.material.transition.platform.MaterialContainerTransform
 import com.google.android.material.transition.platform.MaterialContainerTransformSharedElementCallback
-import com.google.android.material.transition.platform.MaterialSharedAxis
 
 class ProfileActivity : AppCompatActivity() {
 	private lateinit var binding: ActivityProfileBinding
@@ -15,14 +15,13 @@ class ProfileActivity : AppCompatActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		findViewById<View>(android.R.id.content).transitionName = "shared_element"
 		setEnterSharedElementCallback(MaterialContainerTransformSharedElementCallback())
-		window.sharedElementEnterTransition = MaterialContainerTransform().apply {
+		val materialContainerTransform = MaterialContainerTransform().apply {
 			addTarget(android.R.id.content)
-			duration = 300L
+			scrimColor = Color.TRANSPARENT
+			pathMotion = MaterialArcMotion()
 		}
-		window.sharedElementReturnTransition = MaterialContainerTransform().apply {
-			addTarget(android.R.id.content)
-			duration = 250L
-		}
+		window.sharedElementEnterTransition = materialContainerTransform
+		window.sharedElementReturnTransition = materialContainerTransform
 		super.onCreate(savedInstanceState)
 		binding = ActivityProfileBinding.inflate(layoutInflater)
 		setContentView(binding.root)
