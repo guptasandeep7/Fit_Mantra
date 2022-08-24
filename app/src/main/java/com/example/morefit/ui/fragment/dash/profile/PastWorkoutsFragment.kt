4f1cddcb12a8b9942e39667497b98cd673afb38a
Debashish Kundu <debashish.joy@gmail.com>
2022-08-24 23:32:47 +0530

    removed sample data form past workouts

--- app/src/main/java/com/example/morefit/ui/fragment/dash/profile/PastWorkoutsFragment.kt
+++ app/src/main/java/com/example/morefit/ui/fragment/dash/profile/PastWorkoutsFragment.kt
@@ -1,6 +1,7 @@
 package com.example.morefit.ui.fragment.dash.profile
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -10,7 +11,6 @@ import com.example.morefit.adapter.PastWorkoutsInterface
 import com.example.morefit.adapter.PastWorkoutsRecyclerAdapter
 import com.example.morefit.database.ContentApplication
 import com.example.morefit.databinding.FragmentPastWorkoutsBinding
-import com.example.morefit.model.database.Content
 import com.example.morefit.view_models.ContentViewModel
 import com.example.morefit.view_models.WordViewModelFactory
 import com.google.android.material.transition.platform.MaterialSharedAxis
@@ -30,25 +30,25 @@ class PastWorkoutsFragment : Fragment(R.layout.fragment_past_workouts), PastWork
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentPastWorkoutsBinding.bind(view).apply {
-//			contentViewModel.allWords.observe(viewLifecycleOwner) {
-            val data = listOf(
-                Content(1220227200L, "Push-Ups", 120L, 12),
-                Content(1220227211L, "Pull-Ups", 20L, 1256565656),
-                Content(1220627200L, "Tree Pose", 1200000L, 1),
-                Content(1220220200L, "Chin-Ups", 12000L, 33),
-                Content(
-                    1210227200L,
-                    "sffufgdfadfgesucfnseguiescgraugauagdudgaufgfaufgfuagfaufga",
-                    120L,
-                    12
-                ),
-                Content(66220227200L, "a", 10L, 122),
-                Content(1226666666601027200L, "Vraksasana", 1201L, 92),
-            )
-            workoutsRecyclerView.adapter = PastWorkoutsRecyclerAdapter(
-                this@PastWorkoutsFragment, data
-            )
-//			}
+            contentViewModel.allWords.observe(viewLifecycleOwner) {
+//            val data = listOf(
+//                Content(1220227200L, "Push-Ups", 120L, 12),
+//                Content(1220227211L, "Pull-Ups", 20L, 1256565656),
+//                Content(1220627200L, "Tree Pose", 1200000L, 1),
+//                Content(1220220200L, "Chin-Ups", 12000L, 33),
+//                Content(
+//                    1210227200L,
+//                    "sffufgdfadfgesucfnseguiescgraugauagdudgaufgfaufgfuagfaufga",
+//                    120L,
+//                    12
+//                ),
+//                Content(66220227200L, "a", 10L, 122),
+//                Content(1226666666601027200L, "Vraksasana", 1201L, 92),
+//            )
+                Log.e("ROOM_DATA_SIZE", it.size.toString())
+                workoutsRecyclerView.adapter =
+                    PastWorkoutsRecyclerAdapter(this@PastWorkoutsFragment, it)
+            }
 
             topBar.setOnClickListener { findNavController().navigateUp() }
         }
